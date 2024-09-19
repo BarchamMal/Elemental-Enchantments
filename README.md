d8ca3b82543952cd0092fe737c58629a6063b516
BarchamMal <115943779+BarchamMal@users.noreply.github.com>
2024-09-19 16:44:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Elemental Enchantments
+ A Minecraft (fabric?) mod which adds more elemental enchantments to complete the current ones.
---
+++ src/client/java/net/barch/e_enchantments/ElementalEnchantmentsClient.java
@@ -0,0 +1,10 @@
+package net.barch.e_enchantments;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElementalEnchantmentsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
